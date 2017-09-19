b7726b8dc55295817bd104ca1ba928019385e565
chandrabuwana <cbuwana@gmail.com>
2017-09-19 15:51:37 +0700

    linear search Done

--- linear_search.js
+++ linear_search.js
@@ -4,11 +4,21 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var count=0
+  for(let i=0;i<values.length;i++){//melakukan pengecekan
+    if(target===values[i]){//sesuaikan dengan taget yg diiinginkan
+      // target=count
+      // console.log(count);
+      return i//ditampilkan jumlah return
+    }
+  }
+  return -1
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
-
+let random_numbers = [ 6, 29,40, 18, 2, 72, 19, 18, 10, 37 ];
 console.log(linearSearch(18, random_numbers));
+console.log(linearSearch(2, random_numbers));
+
 // 2
 console.log(linearSearch(9, random_numbers));
 // -1
@@ -17,6 +27,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var lokasi=[]
+  for (var i=0;i<values.length;i++){
+    if(target===values[i]){
+      lokasi.push(i)
+    }
+  }
+  return lokasi
 }
 
 let banana_arr = "banana".split("");
