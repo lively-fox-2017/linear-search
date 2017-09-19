8cd0f96929c29334baf3e3c1b4f68a67848a7384
Bambang Kurniawan <bembkurniawan@gmail.com>
2017-09-19 14:13:52 +0700

    complete linearSearch() and globalLinearSearch() functions

--- linear_search.js
+++ linear_search.js
@@ -3,7 +3,10 @@
 // Release 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
-  //write your code here
+	for (let i = 0; i< values.length; i++) {
+		if (values[i] === target) return i;
+	}
+	return -1;
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -16,7 +19,11 @@ console.log(linearSearch(9, random_numbers));
 // Release 1
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
-  //write your code here
+	let result = [];
+	for (let i = 0; i < values.length; i++) {
+		if (values[i] === target) result.push(i);
+	}
+	return result.length > 0 ? result : -1;
 }
 
 let banana_arr = "banana".split("");
