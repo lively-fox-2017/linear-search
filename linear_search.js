5ef1a4ad710b995fb383be2290e3d0b131d1e050
ipunganwar <ipung.anwar1@gmail.com>
2017-09-19 15:54:57 +0700

    release 1

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,9 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for(var i = 0; i < values.length; i++){
+  	return values.indexOf(target)
+  }
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,6 +20,13 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var simpan = [];
+  for(var i = 0; i < values.length; i++){
+  	if(values[i] === target){
+  		simpan.push(i);
+  	}
+  }
+  return simpan;
 }
 
 let banana_arr = "banana".split("");
