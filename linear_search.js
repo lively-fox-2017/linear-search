d7bee3a0327ca7a6dca3d19506cb69ae8426f884
Ian Max <ianmax1983@gmail.com>
2017-09-19 16:26:19 +0700

    release 1:linier

--- linear_search.js
+++ linear_search.js
@@ -4,9 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var x = 0; x < values.length; x++){
+    if (target == values[x])
+    return x;
+  }
+  return -1;
 }
 
-let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
+let random_numbers = [6, 29, 18, 2, 72, 19, 18, 10, 37];
 
 console.log(linearSearch(18, random_numbers));
 // 2
@@ -14,9 +19,16 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
-console.log("Global Linear Search")
+console.log("Global Linear Search");
 let globalLinearSearch = (target, values) => {
   //write your code here
+  let jawab = [];
+  for (var y=0; y<values.length; y++)
+  {
+    if (target == values[y])
+    jawab.push(y);
+  }
+  return jawab;
 }
 
 let banana_arr = "banana".split("");
