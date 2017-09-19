b8f8ce25d8d6419a09e5854e83a6356b2f4962a2
= <pangkeyd@gmail.com>
2017-09-19 19:57:37 +0700

    linearSearchRecursive-dpangkey

--- linear_search.js
+++ linear_search.js
@@ -1,17 +1,34 @@
 'use strict'
 
 // Release 0
+let index = 0
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
-  var res = 0
-  for(var i = 0; i < values.length; i++){
-    if(values[i] === target){
-      res = i
-      return res
-    }
-  }
-  return -1
+
+  // ========== METODE FOR LOOP ==========
+  // var res = 0
+  // for(var i = 0; i < values.length; i++){
+  //   if(values[i] === target){
+  //     res = i
+  //     return res
+  //   }
+  // }
+  // return -1
+
+
+  // ========== METODE RECURSIVE ==========
+  if (values[index] == target) { // 
+    return index
+  } else if(index === values.length) {
+    index = 0
+    return -1
+  } else {
+    index += 1
+    return linearSearch(target, random_numbers)
+  } 
+
+
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -22,19 +39,39 @@ console.log(linearSearch(9, random_numbers));
 // -1
 
 // Release 1
+let temp = 0
+let arr = []
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
-  var arr = []
-  for(var i = 0; i < values.length; i++){
-    if(values[i] === target){
-      arr.push(i)
+  // ========= METODE FORLOOP ==========
+  // var arr = []
+  // for(var i = 0; i < values.length; i++){
+  //   if(values[i] === target){
+  //     arr.push(i)
+  //   }
+  // }
+  // return arr
+
+  // ========== METODE RECURSIVE ==========
+  if(values[temp] === target){ 
+    arr.push(temp)
+    if(temp === values.length -1) {
+      return arr
+    } else {
+      temp += 1
+      return globalLinearSearch(target, values)
     }
+  } else if (temp === values.length){
+    return arr
+  }else{
+    temp += 1
+    return globalLinearSearch(target, values)
   }
-  return arr
+
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
-console.log(globalLinearSearch("a", banana_arr));
+console.log(globalLinearSearch("b", banana_arr));
 // [ 1, 3, 5 ]
