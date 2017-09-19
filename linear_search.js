f53c48f705065d87178a0ac08036221e8c37da67
= <pangkeyd@gmail.com>
2017-09-19 15:14:28 +0700

    linear-search-dpangkey

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,14 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  var res = 0
+  for(var i = 0; i < values.length; i++){
+    if(values[i] === target){
+      res = i
+      return res
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,14 +25,16 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
+  var arr = []
+  for(var i = 0; i < values.length; i++){
+    if(values[i] === target){
+      arr.push(i)
+    }
+  }
+  return arr
 }
 
 let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
 // [ 1, 3, 5 ]
-
-module.exports = {
-  linearSearch,
-  globalLinearSearch
-}
