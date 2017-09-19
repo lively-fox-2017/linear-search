f9627009df35290b7ff2d9585c86b41a37914492
pjap <pranajaya.pratama@gmail.com>
2017-09-19 14:05:31 +0700

    LINEAR SEARCH TASK

--- linear_search.js
+++ linear_search.js
@@ -4,6 +4,12 @@
 console.log("Linear Search")
 let linearSearch = (target, values) => {
   //write your code here
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      return i;
+    }
+  }
+  return -1
 }
 
 let random_numbers = [ 6, 29, 18, 2, 72, 19, 18, 10, 37 ];
@@ -17,14 +23,22 @@ console.log(linearSearch(9, random_numbers));
 console.log("Global Linear Search")
 let globalLinearSearch = (target, values) => {
   //write your code here
-}
+  var tampung = [];
 
-let banana_arr = "banana".split("");
+  for (var i = 0; i < values.length; i++) {
+    if (values[i] === target) {
+      tampung.push(i)
+    }
+  }
+  return tampung
+}
+//
+  let banana_arr = "banana".split("");
 // ["b", "a", "n", "a", "n", "a"]
 console.log(globalLinearSearch("a", banana_arr));
-// [ 1, 3, 5 ]
+// // [ 1, 3, 5 ]
 
 module.exports = {
+  globalLinearSearch,
   linearSearch,
-  globalLinearSearch
 }
